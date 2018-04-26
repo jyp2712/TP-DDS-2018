6816bfcb4111e4411fc40b117adc44e7e3a9afcf
christian <christian@VM>
2018-04-26 20:25:17 -0300

    Agregue todas las instancias de categoria al categorizador

--- src/main/java/tp0/modelo/Categorizador.java
+++ src/main/java/tp0/modelo/Categorizador.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 public class Categorizador {
 	// SM: El nombre FCategorias es por algo? Una convencion?
-	// Se que esta inicializacion se ve fea pero es lo mas simple hasta que tengamos mas informacion.
+	// Se que esta inicializacion se ve fea pero es lo mas simple hasta que tengamos
+	// mas informacion.
 	private List<Categoria> FCategorias = Arrays.asList(new Categoria("R1", 18.76, 0.644, 0, 150),
 			new Categoria("R2", 35.32, 0.644, 150, 325), new Categoria("R3", 60.71, 0.681, 325, 400),
 			new Categoria("R4", 71.74, 0.738, 400, 450), new Categoria("R5", 110.38, 0.794, 450, 500),
 			new Categoria("R6", 220.75, 0.832, 500, 600), new Categoria("R7", 443.59, 0.851, 600, 700),
-			new Categoria("R8", 545.96, 0.851, 700, 1400), new Categoria("R9", 887.19, 0.851, 1400, 999999999/*QUE CARAJOS*/));
+			new Categoria("R8", 545.96, 0.851, 700, 1400),
+			new Categoria("R9", 887.19, 0.851, 1400, Double.POSITIVE_INFINITY));
 
 	/*
 	 * public void addCategoria(Categoria Categoria) { FCategorias.add(Categoria); }
@@ -25,9 +27,10 @@ public class Categorizador {
 	// se tiene que recategorizar pero no especifica
 	// muchos mas que eso... Por ahora opino de dejarlo y ver como avanzan los
 	// requisistos.
-	public void asignarCategoria(Cliente Cliente) {
-		Cliente.setCategoria(this.determinarCategoria(Cliente.Consumo()));
-	}
+	/*
+	 * public void asignarCategoria(Cliente Cliente) {
+	 * Cliente.setCategoria(this.determinarCategoria(Cliente.Consumo())); }
+	 */
 
 	public Categoria determinarCategoria(double Consumo) {
 		return FCategorias.stream()
