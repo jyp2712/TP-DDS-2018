0061d53da6401781e97a16bd9fd1f370f1c84e81
christian <christian@VM>
2018-04-26 21:37:27 -0300

    Quite algunos setters para hacer el dispositivo mas inmutable

--- src/main/java/tp0/modelo/Dispositivo.java
+++ src/main/java/tp0/modelo/Dispositivo.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Dispositivo {
-	
+
 	@JsonProperty
 	protected String nombreGenerico;
 
@@ -13,13 +13,10 @@ public class Dispositivo {
 
 	@JsonProperty
 	protected boolean estado;
-	
+
 	@JsonCreator
-	public Dispositivo(
-			@JsonProperty("nombre generico") String nombreGenerico,
-			@JsonProperty("KW/H") double KwXHora,
-			@JsonProperty("estado") boolean estado
-			) {
+	public Dispositivo(@JsonProperty("nombre generico") String nombreGenerico, @JsonProperty("KW/H") double KwXHora,
+			@JsonProperty("estado") boolean estado) {
 		this.nombreGenerico = nombreGenerico;
 		this.KwXHora = KwXHora;
 		this.estado = estado;
@@ -29,24 +26,31 @@ public class Dispositivo {
 		return nombreGenerico;
 	}
 
-	public void setNombreGenerico(String nombreGenerico) {
+/*	public void setNombreGenerico(String nombreGenerico) {
 		this.nombreGenerico = nombreGenerico;
-	}
+	}*/
 
 	public double getKwXHora() {
 		return KwXHora;
 	}
 
-	public void setKwXHora(double kwXHora) {
+/*	public void setKwXHora(double kwXHora) {
 		KwXHora = kwXHora;
+	}*/
+
+	public boolean getEstado() {
+		return estado;
 	}
 
+	// SM: Por ahora supongo que se puede cambiar el estado. Todavia no sabemos como...
 	public void setEstado(boolean estado) {
 		this.estado = estado;
 	}
 
+	// SM: Un poco raro el pasamanos pero es para que el cliente se desacople de
+	// como esta implementado el estado en el dispositivo
 	public boolean estaEncendido() {
-		return estado;
+		return this.getEstado();
 	}
-	
+
 }
