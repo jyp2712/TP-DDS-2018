46b3a176b38b523a026a3a34952f2e190f1d458a
christian <christian@VM>
2018-04-26 23:51:23 -0300

    Tests para verificar que devuelve la categoria correcta

--- src/test/java/CategorizadorTest.java
+++ src/test/java/CategorizadorTest.java
@@ -62,4 +62,58 @@ public class CategorizadorTest {
 		Categoria categoria = categorizador.determinarCategoria(5000);
 		Assert.assertTrue(categoria.getNombre() == "R9");
 	}
+	
+	@Test
+	public void testDevolverCategoriaR1() {
+		Categoria categoria = categorizador.getCategoria("R1");
+		Assert.assertTrue(categoria.getNombre() == "R1");
+	}
+	
+	@Test
+	public void testDevolverCategoriaR2() {
+		Categoria categoria = categorizador.getCategoria("R2");
+		Assert.assertTrue(categoria.getNombre() == "R2");
+	}
+	
+	@Test
+	public void testDevolverCategoriaR3() {
+		Categoria categoria = categorizador.getCategoria("R3");
+		Assert.assertTrue(categoria.getNombre() == "R3");
+	}
+	
+	@Test
+	public void testDevolverCategoriaR4() {
+		Categoria categoria = categorizador.getCategoria("R4");
+		Assert.assertTrue(categoria.getNombre() == "R4");
+	}
+	
+	@Test
+	public void testDevolverCategoriaR5() {
+		Categoria categoria = categorizador.getCategoria("R5");
+		Assert.assertTrue(categoria.getNombre() == "R5");
+	}
+	
+	@Test
+	public void testDevolverCategoriaR6() {
+		Categoria categoria = categorizador.getCategoria("R6");
+		Assert.assertTrue(categoria.getNombre() == "R6");
+	}
+	
+	@Test
+	public void testDevolverCategoriaR7() {
+		Categoria categoria = categorizador.getCategoria("R7");
+		Assert.assertTrue(categoria.getNombre() == "R7");
+	}
+	
+	@Test
+	public void testDevolverCategoriaR8() {
+		Categoria categoria = categorizador.getCategoria("R8");
+		Assert.assertTrue(categoria.getNombre() == "R8");
+	}
+	
+	@Test
+	public void testDevolverCategoriaR9() {
+		Categoria categoria = categorizador.getCategoria("R9");
+		Assert.assertTrue(categoria.getNombre() == "R9");
+	}
 }
