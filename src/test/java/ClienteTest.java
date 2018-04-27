61e78e4bca355b9ed4dcf00b6629d92c8f4ee46f
nicofonseca47 <nicofonseca47@VM>
2018-04-27 00:00:07 -0300

    Los tests del cliente para evaluar su comportamiento

---
+++ src/test/java/ClienteTest.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+import org.junit.*;
+
+import tp0.modelo.Categoria;
+import tp0.modelo.Cliente;
+import tp0.modelo.Dispositivo;
+
+public class ClienteTest{
+	
+	//Tests para probar el comportamiento del cliente
+	
+	Cliente nico;
+	Dispositivo dispositivo1;
+	Dispositivo dispositivo2;
+	Dispositivo dispositivo3;
+	Dispositivo dispositivo4;
+	Categoria categoriaPrueba;
+	
+	@Before
+	public void setUp() throws Exception{
+		dispositivo1 = new Dispositivo("Heladera", 1234, true);
+		dispositivo2 = new Dispositivo("Lavarropas", 4321, true);
+		dispositivo3 = new Dispositivo("Tostadora", 500, false);
+		dispositivo4 = new Dispositivo("Licuadora", 666, false);
+		nico = new Cliente("Nicolás", "Fonseca", "DNI", 39068888, "1141693939", "Calle Falsa 123", "2018-01-01", "R2", Arrays.asList(dispositivo1, dispositivo2, dispositivo3, dispositivo4));
+		categoriaPrueba = new Categoria("R1", 18.76, 0.644, 0, 150);
+	}
+	
+	/*Comento el test porque no existe más el método
+	 * 
+	 * @Test
+	public void testCambiarDispositivoClienteNico() {
+		nico.setCategoria(categoriaPrueba);
+		Assert.assertTrue(nico.getCategoria().getId() == "R1");
+	}*/
+	
+	@Test
+	public void testNicoTieneAlgunDispositivoEncendido() {
+		Assert.assertTrue(nico.tieneAlgunDispositivoEncendido());
+	}
+	
+	@Test
+	public void testNicoTiene4Dispositivos() {
+		Assert.assertTrue(nico.cantidadDispositivosTotal() == 4);
+	}
+	
+	@Test
+	public void testNicoTiene2DIspositivosEncendidos() {
+		Assert.assertTrue(nico.cantidadDispositivosEncendidos() == 2);
+
+	}
+	@Test
+	public void testNicoTiene2DIspositivosApagados() {
+		Assert.assertTrue(nico.cantidadDispositivosApagados() == 2);
+
+	}
+}
