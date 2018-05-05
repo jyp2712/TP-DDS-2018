ab2b2663a3d2cd92db931784f6460612b30926bb
christian <csmarino90@gmail.com>
2018-05-05 19:04:50 -0300

    Modificacion setters

--- src/main/java/tp0/modelo/Categoria.java
+++ src/main/java/tp0/modelo/Categoria.java
@@ -7,36 +7,56 @@ public class Categoria {
 	private double cargoVariable;
 	private double consumoMinimo;
 	private double consumoMaximo;
-	
+
 	public Categoria(String id, double cargoFijo, double cargoVariable, double consumoMinimo, double consumoMaximo) {
-		this.nombre = id;
-		this.cargoFijo = cargoFijo;
-		this.cargoVariable = cargoVariable;
-		this.consumoMaximo = consumoMaximo;
-		this.consumoMinimo = consumoMinimo;
+		setNombre(id);
+		setCargoFijo(cargoFijo);
+		setCargoVariable(cargoVariable);
+		setConsumoMaximo(consumoMaximo);
+		setConsumoMinimo(consumoMinimo);
 	}
 
 	public String getNombre() {
 		return nombre;
 	}
-	
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 	public double getCargoFijo() {
 		return cargoFijo;
 	}
-	
+
+	private void setCargoFijo(double cargoFijo) {
+		this.cargoFijo = cargoFijo;
+	}
+
 	public double getCargoVariable() {
 		return cargoVariable;
 	}
-	
+
+	private void setCargoVariable(double cargoVariable) {
+		this.cargoVariable = cargoVariable;
+	}
+
 	public double getConsumoMinimo() {
 		return consumoMinimo;
+	}
+
+	private void setConsumoMinimo(double consumoMinimo) {
+		this.consumoMinimo = consumoMinimo;
 	}
 
 	public double getConsumoMaximo() {
 		return consumoMaximo;
 	}
-	
+
+	private void setConsumoMaximo(double consumoMaximo) {
+		this.consumoMaximo = consumoMaximo;
+	}
+
 	public boolean enRango(Double consumo) {
-		return consumo >= getConsumoMinimo() && consumo <= getConsumoMaximo();
+		return consumo >= getConsumoMinimo() && consumo < getConsumoMaximo();
 	}
 }
