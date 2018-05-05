56999c4c4cd16fe824922a54da5db998fd0cf861
jyp2712 <pompeypablo@hotmail.com>
2018-05-05 02:19:42 -0300

    Modifico Test de AdministradoresJson

--- src/test/java/JsonAdministradorTest.java
+++ src/test/java/JsonAdministradorTest.java
@@ -1,18 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.*;
 
 import tp0.modelo.Administrador;
-import tp0.modelo.repositorios.Repositorios;
+import tp0.modelo.DecodificadorJson;
+import tp0.modelo.repositorios.fuentes.FuenteArchivo;
 
 public class JsonAdministradorTest{
 	
 	//Tests para probar la carga de datos desde administradores.json
-	List<Administrador> administradores = Repositorios.obtenerRepositorioDeAdministradores().todos();
+	DecodificadorJson decodificador = new DecodificadorJson(new FuenteArchivo("administradores.json"), Administrador.class);
+	List<Administrador> administradores = new ArrayList<>();
 	Administrador administrador, juan;
 
+	@SuppressWarnings("unchecked")
 	@Before
 	public void setUp() throws Exception {
+		administradores.addAll((Collection<? extends Administrador>) decodificador.leer());
 		administrador = new Administrador(101010, "Juan", "Lopez", "Calle Falsa 123", "2017-07-01");
 		juan = administradores.stream().filter(administrador -> administrador.getId().equals(101010)).findFirst().get();
 	}
