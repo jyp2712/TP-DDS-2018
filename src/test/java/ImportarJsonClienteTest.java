7dd1a0d86f9b6bad1fdc5dec6a7f1f96044daf72
christian <csmarino90@gmail.com>
2018-05-05 00:39:04 -0300

    Test que rehune algunos comportamientos. No se si va a ser definitivo

---
+++ src/test/java/ImportarJsonClienteTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class ImportarJsonClienteTest {
+
+	//Fuente fuente;
+	//CodificadorJson codificadorJson;
+	//Repositorio<Cliente> repositorioClientes;
+
+	@Before
+	public void setUp() throws Exception {
+		//Aca hay que ir pidiendo para lograr tener un repositorio cargado
+	}
+	
+	@Test
+	public void test() {
+		//Algo...
+	}
+
+
+}
