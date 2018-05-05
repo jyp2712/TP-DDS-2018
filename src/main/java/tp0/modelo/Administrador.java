2dfec74a59b10d836f2832adef01a959204ed928
christian <csmarino90@gmail.com>
2018-05-05 19:08:27 -0300

    Creacion setters. Perdon, no me gusta acceder directo al atributo

--- src/main/java/tp0/modelo/Administrador.java
+++ src/main/java/tp0/modelo/Administrador.java
@@ -26,35 +26,54 @@ public class Administrador {
 	public Administrador(@JsonProperty("id") Integer id, @JsonProperty("nombre") String nombre,
 			@JsonProperty("apellido") String apellido, @JsonProperty("domicilio") String domicilio,
 			@JsonProperty("fecha de alta en el sistema") String fechaAltaSistema) {
-		this.id = id;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.domicilio = domicilio;
-		this.fechaAltaSistema = new DateTime(fechaAltaSistema);
+		setId(id);
+		setNombre(nombre);
+		setApellido(apellido);
+		setDomicilio(domicilio);
+		setFechaAltaSistema(new DateTime(fechaAltaSistema));
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
-	
+
+	private void setId(Integer id) {
+		this.id = id;
+	}
+
 	public String getNombre() {
 		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 	public String getApellido() {
 		return apellido;
 	}
 
+	private void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
 	public String getDomicilio() {
 		return domicilio;
+	}
+
+	private void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
 	}
 
 	public DateTime getFechaAltaSistema() {
 		return fechaAltaSistema;
 	}
 
+	private void setFechaAltaSistema(DateTime fechaAltaSistema) {
+		this.fechaAltaSistema = fechaAltaSistema;
+	}
+
 	public Months antiguedadAdministrador() {
 		return Months.monthsBetween(fechaAltaSistema, DateTime.now());
-		//return Years.yearsBetween(fechaAltaSistema, DateTime.now()).getYears();
 	}
 }
