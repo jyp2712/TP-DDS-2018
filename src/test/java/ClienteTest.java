78341462601e05d7877d237fdaf8dd41834f25c5
christian <csmarino90@gmail.com>
2018-05-05 18:40:33 -0300

    Tests de CLIENTE OK

--- src/test/java/ClienteTest.java
+++ src/test/java/ClienteTest.java
@@ -17,7 +17,7 @@ public class ClienteTest {
 	Dispositivo dispositivo2;
 	Dispositivo dispositivo3;
 	Dispositivo dispositivo4;
-	Categoria categoriaPrueba;
+	Categoria categoria;
 	Repositorio<Categoria> categorias;
 
 	@Before
@@ -26,13 +26,13 @@ public class ClienteTest {
 		dispositivo2 = new Dispositivo("Lavarropas", 150, false);
 		dispositivo3 = new Dispositivo("Tostadora", 50, false);
 		dispositivo4 = new Dispositivo("Licuadora", 50, false);
-		categoriaPrueba = new Categoria("R1", 18.76, 0.644, 0, 500);
+		categoria = new Categoria("R1", 18.76, 0.644, 0, 150);
 		categorias = new RepositorioEnMemoria<Categoria>();
 
-		categorias.agregar(categoriaPrueba);
+		categorias.agregar(categoria);
 
 		nico = new Cliente("Nicolás", "Fonseca", "DNI", 39068888, "1141693939", "Calle Falsa 123", "2018-01-01",
-				categorias, Arrays.asList(dispositivo1, dispositivo2, dispositivo3, dispositivo4));
+				categorias, "R1", Arrays.asList(dispositivo1, dispositivo2, dispositivo3, dispositivo4));
 	}
 
 	@Test
@@ -42,26 +42,26 @@ public class ClienteTest {
 
 	@Test
 	public void testNicoTiene4Dispositivos() {
-		Assert.assertEquals(nico.cantidadDispositivosTotal(), 4);
+		Assert.assertEquals(4, nico.cantidadDispositivosTotal());
 	}
 
 	@Test
 	public void testNicoTiene1DIspositivoEncendido() {
-		Assert.assertEquals(nico.cantidadDispositivosEncendidos(), 1);
+		Assert.assertEquals(1, nico.cantidadDispositivosEncendidos());
 	}
 
 	@Test
 	public void testNicoTiene3DIspositivosApagados() {
-		Assert.assertEquals(nico.cantidadDispositivosApagados(), 3);
+		Assert.assertEquals(3, nico.cantidadDispositivosApagados());
 	}
 
 	@Test
-	public void testNicoConsume300KWPorHora() {
-		Assert.assertEquals(nico.consumoEstimadoTotal(), 300);
+	public void testNicoConsume150KWPorHora() {
+		Assert.assertEquals(150, nico.consumoEstimadoTotal(), 0);
 	}
 
 	@Test
 	public void testNicoEsRecategorizadoAR2() {
-		Assert.assertTrue(nico.getCategoria().getNombre() == "R1");
+		Assert.assertEquals("R1", nico.getCategoria().getNombre());
 	}
 }
