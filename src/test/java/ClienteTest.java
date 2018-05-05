0055097c24a8f58f6d0275118a8b69b7d80d4a57
christian <csmarino90@gmail.com>
2018-05-05 18:06:28 -0300

    Adaptados los tests pero aun no corren bien

--- src/test/java/ClienteTest.java
+++ src/test/java/ClienteTest.java
@@ -5,65 +5,63 @@ import org.junit.*;
 import tp0.modelo.Categoria;
 import tp0.modelo.Cliente;
 import tp0.modelo.Dispositivo;
+import tp0.modelo.repositorios.Repositorio;
+import tp0.modelo.repositorios.RepositorioEnMemoria;
 
-public class ClienteTest{
-	
-	//Tests para probar el comportamiento del cliente
-	
+public class ClienteTest {
+
+	// Tests para probar el comportamiento del cliente
+
 	Cliente nico;
 	Dispositivo dispositivo1;
 	Dispositivo dispositivo2;
 	Dispositivo dispositivo3;
 	Dispositivo dispositivo4;
 	Categoria categoriaPrueba;
-	
+	Repositorio<Categoria> categorias;
+
 	@Before
-	public void setUp() throws Exception{
+	public void setUp() throws Exception {
 		dispositivo1 = new Dispositivo("Heladera", 150, true);
-		dispositivo2 = new Dispositivo("Lavarropas", 150, true);
+		dispositivo2 = new Dispositivo("Lavarropas", 150, false);
 		dispositivo3 = new Dispositivo("Tostadora", 50, false);
 		dispositivo4 = new Dispositivo("Licuadora", 50, false);
-		nico = new Cliente("Nicolás", "Fonseca", "DNI", 39068888, "1141693939", "Calle Falsa 123", "2018-01-01", "R2", Arrays.asList(dispositivo1, dispositivo2, dispositivo3, dispositivo4));
-		categoriaPrueba = new Categoria("R1", 18.76, 0.644, 0, 150);
+		categoriaPrueba = new Categoria("R1", 18.76, 0.644, 0, 500);
+		categorias = new RepositorioEnMemoria<Categoria>();
+
+		categorias.agregar(categoriaPrueba);
+
+		nico = new Cliente("Nicolás", "Fonseca", "DNI", 39068888, "1141693939", "Calle Falsa 123", "2018-01-01",
+				categorias, Arrays.asList(dispositivo1, dispositivo2, dispositivo3, dispositivo4));
 	}
-	
-	
-	 
-	@Test
-	public void testCambiarDispositivoClienteNico() {
-		nico.setCategoria(categoriaPrueba);
-		Assert.assertTrue(nico.getCategoria().getNombre() == "R1");
-	}
-	
+
 	@Test
 	public void testNicoTieneAlgunDispositivoEncendido() {
 		Assert.assertTrue(nico.tieneAlgunDispositivoEncendido());
 	}
-	
+
 	@Test
 	public void testNicoTiene4Dispositivos() {
-		Assert.assertTrue(nico.cantidadDispositivosTotal() == 4);
+		Assert.assertEquals(nico.cantidadDispositivosTotal(), 4);
 	}
-	
-	@Test
-	public void testNicoTiene2DIspositivosEncendidos() {
-		Assert.assertTrue(nico.cantidadDispositivosEncendidos() == 2);
 
-	}
 	@Test
-	public void testNicoTiene2DIspositivosApagados() {
-		Assert.assertTrue(nico.cantidadDispositivosApagados() == 2);
-
+	public void testNicoTiene1DIspositivoEncendido() {
+		Assert.assertEquals(nico.cantidadDispositivosEncendidos(), 1);
 	}
-	
+
+	@Test
+	public void testNicoTiene3DIspositivosApagados() {
+		Assert.assertEquals(nico.cantidadDispositivosApagados(), 3);
+	}
+
 	@Test
 	public void testNicoConsume300KWPorHora() {
-		Assert.assertTrue(nico.consumoEstimadoTotal() == 300);
+		Assert.assertEquals(nico.consumoEstimadoTotal(), 300);
 	}
-	
+
 	@Test
 	public void testNicoEsRecategorizadoAR2() {
-		nico.asignarCategoria();
-		Assert.assertTrue(nico.getCategoria().getNombre() == "R2");
+		Assert.assertTrue(nico.getCategoria().getNombre() == "R1");
 	}
 }
