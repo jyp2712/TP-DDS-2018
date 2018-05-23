70ccb587a639bad1267dd5594113f349e85d6184
christian <csmarino90@gmail.com>
2018-05-23 10:47:43 -0300

    Refactor repositorio como abstract class

--- src/main/java/tp0/modelo/repositorios/RepositorioEnMemoria.java
+++ src/main/java/tp0/modelo/repositorios/RepositorioEnMemoria.java
@@ -3,17 +3,20 @@ package tp0.modelo.repositorios;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RepositorioEnMemoria<T> implements Repositorio<T> {
+public class RepositorioEnMemoria<T> extends Repositorio<T> {
 	private List<T> objetos = new ArrayList<>();
 
+	@Override
 	public List<T> todos() {
 		return objetos;
 	}
 
+	@Override
 	public void agregar(T objeto) {
 		objetos.add(objeto);
 	}
 
+	@Override
 	public void remover(T objeto) {
 		objetos.remove(objeto);
 	}
