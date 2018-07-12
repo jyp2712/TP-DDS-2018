47b4edf430cf306a5b8c52a87dc8d7c60d6fbd6a
Alisonnnmichella <Alisonnnmichella@users.noreply.github.com>
2018-07-12 11:01:44 -0300

    agrego unos metodos para simplex

--- src/main/java/tp0/modelo/dispositivo/Dispositivo.java
+++ src/main/java/tp0/modelo/dispositivo/Dispositivo.java
@@ -5,4 +5,6 @@ import org.joda.time.DateTime;
 public interface Dispositivo {
 	public double consumoUltimas(int horas);
 	public double consumoTotal(DateTime fechaInicial, DateTime fechaFinal);
+	public TipoDispositivoEnum getTipoDispositivoEnum();
+	public void setUsoOptimo(double horas);
 }
